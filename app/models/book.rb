5e61cd6ee571c8629e55203c08a1a9fa30e2e673
EC2 Default User <ec2-user@ip-172-31-28-237.ec2.internal>
2022-06-26 14:41:38 +0000

    [Add]コメント機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+  validates :title, presence:true
+  validates :body, presence:true,length:{maximum:200}
+  has_many :book_comments, dependent: :destroy
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id) #favorites(お気に入り)が実在しているか？（user.id)
