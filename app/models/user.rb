4d58e19c906b2b411f3fb245b628327c0aeb466b
EC2 Default User <ec2-user@ip-172-31-28-237.ec2.internal>
2022-06-27 13:27:16 +0000

    [Add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
