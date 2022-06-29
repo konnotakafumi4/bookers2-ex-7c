faa19e73c3372af51a1eb4c9345ad7719727f65f
EC2 Default User <ec2-user@ip-172-31-28-237.ec2.internal>
2022-06-29 04:26:50 +0000

    [Finsih]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/home/about" => "homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   # ネストさせる
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
