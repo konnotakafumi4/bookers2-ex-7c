80080c872b43f132f9b0af5d91e028e988f8eada
EC2 Default User <ec2-user@ip-172-31-28-237.ec2.internal>
2022-06-30 10:27:50 +0000

    [Finish]非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)#book.idも必要
-    comment.book_id = book.id
+    @book = Book.find(params[:book_id]) #@bookに変更
+    comment = current_user.book_comments.new(book_comment_params) #book.idも必要
+    comment.book_id = @book.id #@bookに変更
     comment.save
-    redirect_to book_path(book)
+    #redirect_to book_path(book)
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy #Book_Comment => BookCommentに変更
-    redirect_to book_path(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    #BookComment.find(params[:id]).destroy #Book_Comment => BookCommentに変更
+    #redirect_to book_path(params[:book_id])
   end
 
   private
